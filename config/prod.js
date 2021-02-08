ed8836610610680631ebb1a143c9a3be51c8be94
maria0115 <rkddpdms12@naver.com>
2021-02-08 13:23:25 +0900

    비밀정보보호

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+    "elastic_version":"es6",
+    "elastic_address":{"es6":"http://125.7.235.202:19200"},
+    "default_elastic_id":"elastic",
+    "default_elastic_password":"123!@#qwe"
+};
--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const config = require('./config/key.js');
+
 const app = express()
 const port = 5000
 
+const { Client: Client6 } = require(config.elastic_version)
+const client = new Client6({ node: {
+  url: new URL(config.elastic_address[config.elastic_version]),
+  headers: { 'custom': 'headers' },
+  auth: {
+    username: config.default_elastic_id,
+    password: config.default_elastic_password
+  }
+} })
+
 //application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //application/json
 app.use(bodyParser.json());
@@ -14,16 +26,31 @@ app.get('/', (req, res) => {
 })
 
 app.post('/register', (req, res) => {
-    //회원가입 할때 필요한 정보들을 client에서 가져오면
-    //그것들을 데이터베이스에 넣는다.
-    var qObj = req.body;
-    console.log(qObj);
+  //회원가입 할때 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터베이스에 넣는다.
+  var qObj = req.body;
+  console.log(qObj);
 
-    res.status(200).json({
-        qObj: qObj
-    })
+  run(qObj);
+
+  res.status(200).json({
+    qObj: qObj
   })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+async function run({searchword,fieldname}) {
+  await client.index({
+    index: 'mariatest',
+    type: '_doc', // uncomment this line if you are using Elasticsearch ≤ 6
+    body: {
+      name: searchword,
+      lastname: fieldname
+    }
+  })
+}
+
+run().catch(console.log);
